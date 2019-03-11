c7d3300983115230f4e0c85bfcf5715bad7be12a
JHD <gitkraken.com@dollares.de>
2019-03-11 19:45:43 +0100

    add project data from spring pet clinic deme

---
+++ src/main/java/it/dolling/tryout/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,14 @@
+package it.dolling.tryout.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+}
+
