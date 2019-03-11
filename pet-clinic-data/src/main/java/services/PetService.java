be170b156f0874d16a474e3d2fc499aa090587d4
Jan-Hendrik <git@dolling.it>
2019-03-11 19:57:43 +0100

    Implement Interface for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/services/PetService.java
@@ -0,0 +1,14 @@
+package services;
+
+import it.dolling.tryout.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
