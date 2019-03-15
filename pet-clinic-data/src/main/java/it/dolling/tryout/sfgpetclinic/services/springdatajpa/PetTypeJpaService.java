fe9cafe63e02531e16411dfe2dd1955c3793d6ac
Jan-Hendrik Dolling <git@dolling.it>
2019-03-15 05:10:00 +0100

    Added spring data jpa service for petType. Closes #42

---
+++ pet-clinic-data/src/main/java/it/dolling/tryout/sfgpetclinic/services/springdatajpa/PetTypeJpaService.java
@@ -0,0 +1,16 @@
+package it.dolling.tryout.sfgpetclinic.services.springdatajpa;
+
+import it.dolling.tryout.sfgpetclinic.model.PetType;
+import it.dolling.tryout.sfgpetclinic.repositories.PetTypeRepository;
+import it.dolling.tryout.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeJpaService extends AbstractJpaService<PetType, Long> implements PetTypeService {
+
+    public PetTypeJpaService(PetTypeRepository petTypeRepository) {
+        super(petTypeRepository);
+    }
+}
