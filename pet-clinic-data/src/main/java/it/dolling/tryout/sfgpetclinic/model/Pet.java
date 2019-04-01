a9a6ff50d3cf041197f39f943f6e63ed52f31826
JanHendrikDolling <git@dolling.it>
2019-04-01 20:10:10 +0200

    Added Visit Controller and mappings. Closes #57

--- pet-clinic-data/src/main/java/it/dolling/tryout/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/it/dolling/tryout/sfgpetclinic/model/Pet.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.support.MutableSortDefinition;
+import org.springframework.beans.support.PropertyComparator;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -14,7 +16,10 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Setter
@@ -54,6 +59,29 @@ public class Pet extends BaseEntity {
 
     boolean isInvalid() {
         return getId() == null || getOwner() == null || getOwner().isInvalid();
+    }
+
+    protected Set<Visit> getVisitsInternal() {
+        if (this.visits == null) {
+            this.visits = new HashSet<>();
+        }
+        return this.visits;
+    }
+
+    protected void setVisitsInternal(Set<Visit> visits) {
+        this.visits = visits;
+    }
+
+    public List<Visit> getVisits() {
+        List<Visit> sortedVisits = new ArrayList<>(getVisitsInternal());
+        PropertyComparator.sort(sortedVisits,
+                new MutableSortDefinition("date", false, false));
+        return Collections.unmodifiableList(sortedVisits);
+    }
+
+    public void addVisit(Visit visit) {
+        getVisitsInternal().add(visit);
+        visit.setPet(this);
     }
 
 }
